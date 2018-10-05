d7f4710b01751031d657f0a7785cb238e711f825
KjellConnelly <kjellapps@gmail.com>
2018-10-05 09:35:30 -0700

    updated

--- generateDBTCode.js
+++ generateDBTCode.js
@@ -2,9 +2,37 @@ const https = require('https')
 const url = "https://raw.githubusercontent.com/KjellConnelly/QuickHOTSPublic/master/codes.json"
 
 function getCodesAsObject() {
-  https.get(url, res=>{
-    console.log(res)
+  return new Promise((resolve, reject)=>{
+    https.get(url, res=>{
+      let data = ""
+      res.on('data', chunk=>{
+        data += chunk
+      })
+      res.on('end', ()=>{
+        const codes = JSON.parse(data)
+        resolve(codes)
+      })
+    })
   })
 }
 
-getCodesAsObject()
+function addCodeToCodes(inputCodes, email) {
+  let codes = JSON.parse(JSON.stringify(inputCodes))
+  codes.push({
+    one:email,
+    two:Math.random().toString(36).slice(-8)
+  })
+  return codes
+}
+
+async function start(email) {
+  try {
+    const codes = await getCodesAsObject()
+    const modifiedCodes = addCodeToCodes(codes, email)
+    console.log(JSON.stringify(modifiedCodes))
+  } catch(err) {
+    console.log("start() error: " + err)
+  }
+}
+
+start("dog@dog.com")
